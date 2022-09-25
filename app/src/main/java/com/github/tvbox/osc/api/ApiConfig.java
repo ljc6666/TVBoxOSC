3db331adf9792f39d7f33a9d77ef7c83a47c00c2
ljc6666 <73317485+ljc6666@users.noreply.github.com>
2022-09-25 16:46:24 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://agit.ai/ljc6666/TVBox/src/branch/master/jinnuo.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
